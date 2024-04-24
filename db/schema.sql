ca500a82f3e3ff0266d2e6e540623613208841cc
[j chun] <[justinchun224@gmail.com]>
2024-04-23 18:09:08 -0700

    created budget table schema

--- db/schema.sql
+++ db/schema.sql
@@ -31,3 +31,22 @@ CREATE TABLE expenses (
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
+
+CREATE TABLE budget (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    category_id INT,
+    total_monthly BOOL NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    CHECK (total_monthly = FALSE OR (total_monthly = TRUE AND category_id IS NULL))
+);
+
+-- Insert a category budget
+-- INSERT INTO budget (date, category_id, total_monthly)
+-- VALUES ('2024-04-01', 1, FALSE);
+
+-- Insert a total monthly budget
+-- INSERT INTO budget (date, total_monthly)
+-- VALUES ('2024-04-01', TRUE);
