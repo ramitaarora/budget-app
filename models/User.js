89fb2aa47e07b26e94b91691cd869756b8cbfaf6
[j chun] <[justinchun224@gmail.com]>
2024-04-25 14:50:37 -0700

    added validation for email and password

--- models/User.js
+++ models/User.js
@@ -21,15 +21,36 @@ User.init(
         },
         email: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false
-        },
-        location: {
-            type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isLongEnough: function (val) {
+                    if (val.length < 8) {
+                        throw new Error("Password must be at least 8 characters long")
+                    }
+                },
+                hasNumber: function (val) {
+                    if (!/\d/.test(val)) {
+                        throw new Error("Password must contain at least one number");
+                    }
+                },
+                hasSpecialCharacter: function (val) {
+                    if (!/[!@#$%^&*(),.?":{}|<>]/.test(val)) {
+                        throw new Error("Password must contain at least one special character");
+                    }
+                }
+            },
+            location: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
         }
     },
     {
