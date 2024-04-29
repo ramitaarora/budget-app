dde8a4ec57bb09e4b1e3cce143c048651d812ad7
[j chun] <[justinchun224@gmail.com]>
2024-04-29 10:01:09 -0700

    created pages for NextJS routes

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <title>Budget Planner App</title>
+            </Head>
+            <div>HomePage</div>
+        </>
+    )
+};
+
+export default Home;
