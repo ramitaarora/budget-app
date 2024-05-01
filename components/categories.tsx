679a4ef39319b84696c8cfacf3535485ae01a08d
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-05-01 15:30:11 -0700

    rendering categories and fixed tailwind configuation

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,7 +1,209 @@
+import { useState, useEffect } from 'react';
+
+interface Category {
+    id: number,
+    name: string,
+    parent_id: number | null,
+    color: string,
+    flexible: boolean,
+    budget_amount: number,
+    date: string,
+    account_id: number,
+}
+
+const categoryData: Category[] = [
+    {
+        id: 1,
+        name: "Food",
+        parent_id: null,
+        color: "blue",
+        flexible: true,
+        budget_amount: 1000,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 2,
+        name: "Groceries",
+        parent_id: 1,
+        color: "blue",
+        flexible: true,
+        budget_amount: 600,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 3,
+        name: "Restaurants",
+        parent_id: 1,
+        color: "blue",
+        flexible: true,
+        budget_amount: 400,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 4,
+        name: "Rent",
+        parent_id: null,
+        color: "red",
+        flexible: false,
+        budget_amount: 2500,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 5,
+        name: "Bills",
+        parent_id: null,
+        color: "green",
+        flexible: true,
+        budget_amount: 500,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 6,
+        name: "Phone",
+        parent_id: 5,
+        color: "green",
+        flexible: true,
+        budget_amount: 100,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 7,
+        name: "Water/Electricity",
+        parent_id: 5,
+        color: "green",
+        flexible: true,
+        budget_amount: 200,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 8,
+        name: "Gas",
+        parent_id: 5,
+        color: "green",
+        flexible: true,
+        budget_amount: 30,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 9,
+        name: "Transportation",
+        parent_id: null,
+        color: "yellow",
+        flexible: true,
+        budget_amount: 700,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 10,
+        name: "Gas",
+        parent_id: 9,
+        color: "yellow",
+        flexible: true,
+        budget_amount: 300,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 11,
+        name: "Car Payment",
+        parent_id: 9,
+        color: "yellow",
+        flexible: true,
+        budget_amount: 400,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 12,
+        name: "Necessities",
+        parent_id: null,
+        color: "pink",
+        flexible: true,
+        budget_amount: 500,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 13,
+        name: "Entertainment",
+        parent_id: null,
+        color: "black",
+        flexible: true,
+        budget_amount: 100,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 14,
+        name: "Holidays/Gifts",
+        parent_id: null,
+        color: "orange",
+        flexible: true,
+        budget_amount: 300,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 15,
+        name: "Medical",
+        parent_id: null,
+        color: "pink",
+        flexible: true,
+        budget_amount: 100,
+        date: "05-2024",
+        account_id: 1
+    },
+    {
+        id: 16,
+        name: "Misc",
+        parent_id: null,
+        color: "purple",
+        flexible: true,
+        budget_amount: 100,
+        date: "05-2024",
+        account_id: 1
+    }
+]
+
 export default function Categories() {
+    const [parentCategory, setParentCategory] = useState<Category[]>([]);
+    const [childCategory, setChildCategory] = useState<Category[]>([]);
+
+    useEffect(() => {
+        const parentCategories = categoryData.filter((category) => category.parent_id === null);
+        const childCategories = categoryData.filter((category) => category.parent_id !== null);
+
+        setParentCategory(parentCategories);
+        setChildCategory(childCategories);
+    }, [categoryData])
+
     return (
         <section id="categories">
-            <h2>Categories</h2>
+            <h2 className="text-center">Categories</h2>
+            {parentCategory.length ? parentCategory.map((parentItem, parentIndex) => (
+                <div key={parentIndex}>
+                    <div>
+                        <p className="font-bold">{parentItem.name}</p>
+                        {childCategory
+                            .filter((childItem) => childItem.parent_id === parentItem.id)
+                            .map((filteredChildItem, childIndex) => (
+                                <div key={childIndex} className="mx-3">
+                                    <p>{filteredChildItem.name}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            )) : null}
         </section>
     );
 }
