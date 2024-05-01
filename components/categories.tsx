1ee2d3e6980e8e41ad893bf7a7fde64078743666
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-05-01 15:48:08 -0700

    added progress bars

--- components/categories.tsx
+++ components/categories.tsx
@@ -177,10 +177,17 @@ const categoryData: Category[] = [
 export default function Categories() {
     const [parentCategory, setParentCategory] = useState<Category[]>([]);
     const [childCategory, setChildCategory] = useState<Category[]>([]);
+    const [totalBudget, setTotalBudget] = useState<number>(0);
 
     useEffect(() => {
+        setTotalBudget(0);
         const parentCategories = categoryData.filter((category) => category.parent_id === null);
         const childCategories = categoryData.filter((category) => category.parent_id !== null);
+        const budgetAmount = categoryData.forEach((category) => {
+            if (category.parent_id === null) {
+                setTotalBudget((pre) => pre + category.budget_amount);
+            }
+        })
 
         setParentCategory(parentCategories);
         setChildCategory(childCategories);
@@ -191,17 +198,23 @@ export default function Categories() {
             <h2 className="text-center">Categories</h2>
             {parentCategory.length ? parentCategory.map((parentItem, parentIndex) => (
                 <div key={parentIndex}>
-                    <div>
-                        <p className="font-bold">{parentItem.name}</p>
-                        {childCategory
-                            .filter((childItem) => childItem.parent_id === parentItem.id)
-                            .map((filteredChildItem, childIndex) => (
-                                <div key={childIndex} className="mx-3">
-                                    <p>{filteredChildItem.name}</p>
-                                </div>
-                            ))
-                        }
+                    <div className="flex w-100 justify-between items-center">
+                        <p className="font-bold mr-5">{parentItem.name}</p>
+                        <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                            <div className="bg-blue-600 h-2.5 rounded-full" style={{ "width": `${(parentItem.budget_amount/totalBudget) * 100}%` }}></div>
+                        </div>
                     </div>
+                    {childCategory
+                        .filter((childItem) => childItem.parent_id === parentItem.id)
+                        .map((filteredChildItem, childIndex) => (
+                            <div key={childIndex} className="ml-3 flex w-100 justify-between items-center">
+                                <p className="mr-5">{filteredChildItem.name}</p>
+                                <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                                    <div className="bg-blue-600 h-2.5 rounded-full" style={{ "width": `${(filteredChildItem.budget_amount / parentItem.budget_amount) * 100}%` }}></div>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             )) : null}
         </section>
