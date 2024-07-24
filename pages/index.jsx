7ee0b3c1cc5d7bd2dae87adde6a90fef2ba4aff2
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-23 21:57:18 -0700

    homepage nav

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,25 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+    const router = useRouter();
+
+    const loginPage = () => {
+        router.push('/login');
+    }
+
     return (
         <div>
             <Head>
                 <title>Budget Planner App</title>
             </Head>
-            <div>HomePage</div>
+            <nav className="p-2 w-screen flex justify-evenly align-center">
+                <button onClick={loginPage}>Login</button>
+                <button>Logout</button>
+            </nav>
+            <header className="text-center">
+                <h1>Budget Planner App</h1>
+            </header>
         </div>
     )
 }
