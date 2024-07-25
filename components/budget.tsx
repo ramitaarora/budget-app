9133c4abe8b517f0d8da41a01c15c952ced6d354
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-25 10:47:45 -0700

    budget modal

--- components/budget.tsx
+++ components/budget.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import AddBudget from './add-budget';
 
 export default function Budget() {
     const [expensesData, setExpensesData] = useState<any[]>([]);
@@ -6,6 +7,7 @@ export default function Budget() {
     const [incomeData, setIncomeData] = useState<any[]>([]);
     const [totalExpenses, setTotalExpenses] = useState<number>(0);
     const [totalIncome, setTotalIncome] = useState<number>(0);
+    const [modalVisibility, setModalVisibility] = useState<string>('hidden');
 
     const today = new Date();
     const month = today.getMonth() + 1;
@@ -80,11 +82,16 @@ export default function Budget() {
         setTotalIncome(incomeData.reduce((acc, obj) => acc + Number(obj.amount), 0));
     }, [expensesData, incomeData]);
 
+    const openModal = () => {
+        setModalVisibility('visible');
+    }
+
     return (
         <section id="budget">
+            <AddBudget modalVisibility={modalVisibility} setModalVisibility={setModalVisibility} />
             <div className="card-header">
                 <h2>Budget</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" onClick={openModal}>
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                 </svg>
             </div>
