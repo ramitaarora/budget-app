9eb1b084a2d32c98e697a10ae160d1b7a15b6640
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-25 10:20:06 -0700

    added card headers

--- components/categories.tsx
+++ components/categories.tsx
@@ -80,9 +80,14 @@ export default function Categories({ fullDate }: CategoriesProps) {
 
     return (
         <section id="categories">
-                {categoryData.length && expensesData.length ? (
+            <div className="card-header">
+                <h2>Categories</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+            </div>
+            {categoryData.length && expensesData.length ? (
                 <div>
-                    <h2 className="text-center">Categories</h2>
                     {parentCategory.length ? parentCategory.map((parentItem, parentIndex) => (
                         <div key={parentIndex}>
                             <div className="flex w-100 justify-between items-center">
@@ -105,7 +110,7 @@ export default function Categories({ fullDate }: CategoriesProps) {
                         </div>
                     )) : null}
                 </div>
-                ) : null}
+            ) : null}
         </section>
     );
 }
