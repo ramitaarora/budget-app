6d0e29d166daa730d4cdec32437ed91904bc5138
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-25 10:51:16 -0700

    expenses modal

--- components/add-expense.jsx
+++ components/add-expense.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import DateSelector from './date-selector';
 import CurrencyInput from 'react-currency-input-field';
 
-export default function AddExpense() {
+export default function AddExpense({ modalVisibility, setModalVisibility }) {
 
     const [formState, setFormState] = useState({ description: '', date: '', amount: '', category_id: '' });
 
@@ -43,46 +43,57 @@ export default function AddExpense() {
         }
     };
 
+    const closeModal = () => {
+        setModalVisibility('hidden');
+    }
+
     return (
-        <div>
-            <form onSubmit={handleFormSubmit}>
-                <p>Add Expense</p>
-                <div>
-                    <input
-                        placeholder="Description"
-                        name="description"
-                        type="text"
-                        value={formState.description}
-                        onChange={handleChange}
-                    />
-                    <label>Category:</label>
-                    <select
-                        name="category_id"
-                        value={formState.category_id}
-                        onChange={handleChange}
-                    >
-                        <option value="1">Food</option>
-                        <option value="4">Rent</option>
-                        <option value="5">Bills</option>
-                        <option value="6">Transporation</option>
-                        <option value="9">Necessities</option>
-                        <option value="10">Entertainment</option>
-                        <option value="11">Holiday/Gifts</option>
-                        <option value="12">Medical</option>
-                        <option value="13">Misc.</option>
-                    </select>
-                    <DateSelector date={formState.date} onChange={handleChange} />
-                    <label>Amount: </label>
-                    <CurrencyInput
-                        name="amount"
-                        prefix="$"
-                        defaultValue={0}
-                        decimalsLimit={2}
-                        onChange={handleChange}
-                    />
+        <div className={"modal-background " + modalVisibility}>
+            <div className="modal">
+                <div className="modal-content">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" onClick={closeModal} className="exit">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+                    </svg>
+                    <form onSubmit={handleFormSubmit}>
+                        <p>Add Expense</p>
+                        <div>
+                            <input
+                                placeholder="Description"
+                                name="description"
+                                type="text"
+                                value={formState.description}
+                                onChange={handleChange}
+                            />
+                            <label>Category:</label>
+                            <select
+                                name="category_id"
+                                value={formState.category_id}
+                                onChange={handleChange}
+                            >
+                                <option value="1">Food</option>
+                                <option value="4">Rent</option>
+                                <option value="5">Bills</option>
+                                <option value="6">Transporation</option>
+                                <option value="9">Necessities</option>
+                                <option value="10">Entertainment</option>
+                                <option value="11">Holiday/Gifts</option>
+                                <option value="12">Medical</option>
+                                <option value="13">Misc.</option>
+                            </select>
+                            <DateSelector date={formState.date} onChange={handleChange} />
+                            <label>Amount: </label>
+                            <CurrencyInput
+                                name="amount"
+                                prefix="$"
+                                defaultValue={0}
+                                decimalsLimit={2}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <button type="submit">Save</button>
+                    </form>
                 </div>
-                <button type="submit">Save</button>
-            </form>
+            </div>
         </div>
     )
 }
--- components/expenses.tsx
+++ components/expenses.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
+import AddExpense from './add-expense';
 
 export default function Expenses() {
     const [expensesData, setExpensesData] = useState<any[]>([]);
+    const [modalVisibility, setModalVisibility] = useState<string>('hidden');
 
     const today = new Date();
     const month = today.getMonth() + 1;
@@ -30,11 +32,16 @@ export default function Expenses() {
         fetchExpenses();
     }, []);
 
+    const openModal = () => {
+        setModalVisibility('visible');
+    }
+
     return (
         <section id="expenses">
+            <AddExpense modalVisibility={modalVisibility} setModalVisibility={setModalVisibility} />
             <div className="card-header">
                 <h2>Latest Expenses</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" onClick={openModal}>
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                 </svg>
             </div>
