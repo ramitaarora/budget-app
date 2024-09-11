3501b9d723e696dd38b9210f3792d685417fe5bc
[j chun] <[justinchun224@gmail.com]>
2024-09-11 16:55:09 -0700

    fixed budget api

--- pages/api/budget.js
+++ pages/api/budget.js
@@ -48,9 +48,15 @@ export async function getBudget(req, res) {
     if (id) query.where.id = id;
     if (category_id) query.where.category_id = category_id;
     if (month && year) {
-        const formattedMonth = String(parseInt(month)).padStart(2, '0');
-        const formattedDate = `${formattedMonth}-01-${year}`;
-        query.where.date = formattedDate;
+        query.where = {
+            ...query.where,
+            date: {
+                [Op.and]: [
+                    literal(`YEAR(date) = ${year}`),
+                    literal(`MONTH(date) = ${month}`)
+                ]
+            }
+        };
     }
 
     try {
