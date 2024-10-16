317047c19df6eb818ab9da13b465822371c1a3d5
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-10-15 17:07:40 -0700

    added button for demo dashboard on login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -72,6 +72,7 @@ export default function Login() {
                 </div>
                 <button type="submit" className="user-submit">Submit</button>
             </form>
+            <button>Demo Dashboard</button>
         </main>
     )
 }
