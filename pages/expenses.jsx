85d632dce8217b1fd37413f23c0efe2f172b5e7e
[j chun] <[justinchun224@gmail.com]>
2024-10-22 00:36:33 -0700

    added button to toggle bulk edit feature

--- pages/expenses.jsx
+++ pages/expenses.jsx
@@ -14,6 +14,11 @@ export default function Expenses() {
     const [editID, setEditID] = useState();
     const [selectedExpenses, setSelectedExpenses] = useState([]);
     const [categoryNames, setCategoryNames] = useState([]);
+    const [bulkEdit, setBulkEdit] = useState(false);
+
+    const chooseBulkEdit = () => {
+        setBulkEdit(prevBulkEdit => !prevBulkEdit);
+    };
 
     useEffect(() => {
         const today = new Date();
@@ -175,14 +180,18 @@ export default function Expenses() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" onClick={openAddModal} className="cursor-pointer">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                     </svg>
-                    <button onClick={openBulkEditModal}>Bulk Edit</button>
+                    <button onClick={chooseBulkEdit}>Edit Multiple?</button>
+                    {
+                        bulkEdit &&
+                        <button onClick={openBulkEditModal}>Bulk Edit</button>
+                    }
                 </div>
                 {expenseData.length > 0 ? (
                     <>
                         <table>
                             <thead>
                                 <tr>
-                                    <th>Select</th>
+                                    {bulkEdit && <th>Select</th>}
                                     <th>Category</th>
                                     <th>Date</th>
                                     <th>Description</th>
@@ -194,13 +203,16 @@ export default function Expenses() {
                             <tbody>
                                 {expenseData.map((expense, index) => (
                                     <tr key={index}>
-                                        <td>
-                                            <input
-                                                type="checkbox"
-                                                checked={selectedExpenses.includes(expense.id)}
-                                                onChange={() => handleSelectExpense(expense.id)}
-                                            />
-                                        </td>
+                                        {
+                                            bulkEdit &&
+                                            <td>
+                                                <input
+                                                    type="checkbox"
+                                                    checked={selectedExpenses.includes(expense.id)}
+                                                    onChange={() => handleSelectExpense(expense.id)}
+                                                />
+                                            </td>
+                                        }
                                         <td>{getCategoryName(expense.category_id)}</td>
                                         <td>{formatDate(expense.date)}</td>
                                         <td>{expense.description}</td>
