20af42e5dcaa3eb29a291d46ee35003374d77782
Ramita <ramitaarora23@gmail.com>
2024-12-19 13:51:39 -0800

    updated to db_pass

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
     process.env.DEPLOYMENT_URL,
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASS,
     {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
